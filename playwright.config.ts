216c0c6cb525cca9c2e77ad2b8ddad43f1df261a
prabhakaran-sankar <210509415+prabhakaran-sankar@users.noreply.github.com>
2026-09-24 11:10:48 +0100

    Increase timeouts for CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 60000,
+  expect: { timeout: 15000 },
   reporter: [['html', { open: 'always' }]],
   use: {
     baseURL: 'https://sandbox-app.brighthr.com/lite',
